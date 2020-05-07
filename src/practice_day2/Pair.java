e45641a421265089d22d9d7300749eddf8d73fb9
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-07 15:58:13 +0900

    インターン二日目の演習課題の追加

---
+++ src/practice_day2/Pair.java
@@ -0,0 +1,30 @@
+package practice_day2;
+
+public class Pair {
+	
+	private int first;
+	private int second;
+	
+	public Pair(int first, int second) {
+		this.first = first;
+		this.second = second;
+	}
+	
+	public void getFirst(int first) {
+		this.first = first;
+	}
+
+
+	public void getSecond(int second) {
+		this.second = second;
+	}
+
+
+	public static void main(String[] args) {
+				
+		
+	
+	}
+
+
+}
