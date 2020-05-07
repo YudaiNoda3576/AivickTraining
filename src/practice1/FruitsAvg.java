354213e91003f4804134acc604000625ac92c41f
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-07 15:53:15 +0900

    メソッドの修正

--- src/practice1/FruitsAvg.java
+++ src/practice1/FruitsAvg.java
@@ -1,6 +1,8 @@
 package practice1;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -15,7 +17,17 @@ public class FruitsAvg {
 			fruits.put("みかん", 150);
 			fruits.put("ぶどう", 300);
 			fruits.put("もも", 400);			
+			
+		System.out.println("以下のフルーツの名前を複数入力してください。平均価格を表示します。区切り文字には「,」を使用してください");
+		System.out.println("りんご、バナナ、みかん、ぶどう、もも");
 
+		// 入力値が複数か判定するようにする		
+		String[]fruit = scanner.next().split(",");		
+		int sum = 0;
+		
+		for(int i = 0; i < fruit.length; i++) {
+			sum += fruits.get(fruit[i]);
+		}
+		System.out.println(sum / fruit.length);
 	}
-
 }
