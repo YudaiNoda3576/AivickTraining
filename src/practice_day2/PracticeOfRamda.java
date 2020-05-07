f98de03aae1f33cb531c08ef9c31c8e704c47747
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-07 21:57:45 +0900

    ラムダ式の練習用クラスの作成

---
+++ src/practice_day2/PracticeOfRamda.java
@@ -0,0 +1,26 @@
+package practice_day2;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class PracticeOfRamda {
+	public static void main(String[]args) {
+//		Java8以前のコレクションをソートする匿名クラス
+//		Java8以降はComparatorのような実装すべきメソッドが一つのインターフェイスのことを関数型インターフェイスという
+//		関数型インターフェイスを実装したクラスの代わりにラムダ式を利用できる
+		List<String>list1 = Arrays.asList("Java", "Scala", "Groovy");
+		list1.sort(new Comparator<String>(){
+			@Override
+			public int compare(String s1, String s2) {
+				return s1.length() - s2.length();
+			}
+		});
+//		上の処理をラムダ式で記述
+		List<String> list2 = Arrays.asList("Java", "Scala", "Groovy");
+		list2.sort((String s1, String s2) -> s1.length() - s2.length());
+		
+		
+	}
+	
+}
