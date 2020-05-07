02b374c5b151d906b5b62598a64a690e00f2f427
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-07 15:57:18 +0900

    新規クラスの追加

---
+++ src/practice1/SearchFruits.java
@@ -0,0 +1,23 @@
+package practice1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SearchFruits {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	
+		Map<Integer, String>fruits = new HashMap<>();
+			fruits.put(200, "りんご");
+			fruits.put(100, "バナナ");
+			fruits.put(150, "みかん");
+			fruits.put(300, "ぶどう");
+			fruits.put(400, "もも");		
+	
+			int fruitPrice = scanner.nextInt();
+				
+			}
+	
+	
+}
