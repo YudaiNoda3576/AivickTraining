54c3e596bbf18608d5afd30f8fd0ff5a225ace21
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-09 18:45:35 +0900

    Averageクラスの修正

--- src/practice1/Average.java
+++ src/practice1/Average.java
@@ -6,18 +6,13 @@ import java.math.RoundingMode;
 public class Average {
 
 	public static void main(String[] args) {
-		double num1 = 1;
-		double num2 = 2;
-		double num3 = 4;
-		double num4 = 20;
+		int sum = 0;
+		for (String arg : args) {
+			sum += Integer.parseInt(arg);
+		}
 		
-		double ave1 = (num1 + num2) / 2;
-		double ave2 = (num2 + num3 + num4) / 3;
+		double average = sum / args.length;
 		
-		BigDecimal bd = new BigDecimal(ave2);
-		BigDecimal bd1 = bd.setScale(1, RoundingMode.UP);
-		
-		System.out.println(ave1);
-		System.out.println(bd1.doubleValue());	
+		System.out.println(average);
 	}
 }
