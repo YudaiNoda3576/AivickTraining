8c71ddbd99a63490122a77681ef42664765d907a
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-09 17:53:44 +0900

    メソッドの修正

--- src/practice_day2/Pair_pattern2.java
+++ src/practice_day2/Pair_pattern2.java
@@ -1,7 +1,9 @@
 package practice_day2;
 
 import java.util.Objects;
-
+//ジェネリクス（総称型）
+//ジェネリッククラスの定義　タプル：複数かつ異なる要素に順序をつけて格納できるデータ構造
+//　S,Tは仮型パラメータ　＝　型安全を保証する
 public class Pair_pattern2<S, T> {
 	private final S first;
 	private final T second;
@@ -10,7 +12,7 @@ public class Pair_pattern2<S, T> {
 		this.first = first;
 		this.second = second;
 	}
-	
+//	ジェネリックメソッド　＝　異なる型（型パラメータで表現）に対して同じ振舞いをするメソッド
 	public S first() {
 		return this.first;
 	}
@@ -25,13 +27,17 @@ public class Pair_pattern2<S, T> {
 			return true;
 		if(o == null || getClass() != o.getClass())
 			return false;
+//		なんらかの型の要素を持つListを表す。このような型を非境界ワイルドカード型
 		Pair_pattern2<?, ?> pair = (Pair_pattern2<?, ?>) o;			
 		return Objects.equals(first, second) &&
 		Objects.equals(second, pair.second);
 	}
-	
+//	equalsメソッドを実装した場合は、hashCodeメソッドも実装する必要がある
+//	equalsメソッドの結果がtrueとなるオブジェクトは、hashCodeメソッド呼び出しの結果同じ値を返す必要がある。 
+//	(equalsがfalseとなるオブジェクトが同じhashCodeの結果を返すことは、かまわない)
 	@Override
 	public int hashCode() {
+//	
 		return Objects.hash(first, second);
 	}
 
