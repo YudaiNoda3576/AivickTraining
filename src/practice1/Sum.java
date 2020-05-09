3b4af1ca63fbb099c5aa92643178f8a66170f725
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-09 18:45:47 +0900

    Sumクラスの修正

--- src/practice1/Sum.java
+++ src/practice1/Sum.java
@@ -3,14 +3,8 @@ package practice1;
 public class Sum {
 
 	public static void main(String[] args) {
-		int num1 = 1;
-		int num2 = 2;
-		int num3 = 3;
-		int num4 = 5;
+		int result = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
 		
-		System.out.println(num1 + num2);
-		System.out.println(num3 + num4);
-
 	}
 
 }
