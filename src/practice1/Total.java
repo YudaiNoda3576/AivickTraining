5d09e31abaec0556ca20cd981cb83f143bfe94b6
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-09 18:45:05 +0900

    Totalクラスの修正

--- src/practice1/Total.java
+++ src/practice1/Total.java
@@ -3,15 +3,19 @@ package practice1;
 public class Total {
 
 	public static void main(String[] args) {
-		int num1 = 1;
-		int num2 = 2;
-		int num3 = 3;
-		int num4 = 4;
-		
-		System.out.println(num1 + num2);
-		System.out.println(num2 + num3 + num4);
-		System.out.println(0);
+		int result = total(args);
+		System.out.println(result);
 
+	}
+		
+	public static int total(String [] nums) {
+		int result = 0;
+		for(String num : nums) {
+			result += Integer.parseInt(num);
+			
+		}
+		return result;
 	}
 
 }
+
