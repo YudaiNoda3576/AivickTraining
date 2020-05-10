82498f19a7fd02b173e905b46769c412dc62b576
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-10 09:58:57 +0900

    Concat.javaの修正

--- src/practice1/Concat.java
+++ src/practice1/Concat.java
@@ -3,16 +3,15 @@ package practice1;
 public class Concat {
 
 	public static void main(String[] args) {
-	 String chars1 = "abc";
-	 String chars2 = "edf";
-	 String text1 = "i";
-	 String text2 = "Love";
-	 String text3 = "java";
-	 
-	 System.out.println(chars1 + chars2);
-	 System.out.println(text1 + text2 + text3);
-	 
+		String concat = "";
 
+		for (String arg : args) {
+			concat += arg;
+		}
+		if (concat == "") {
+			System.out.println("文字列が入力されていません");
+		} else {
+			System.out.println(concat);
+		}
 	}
-
 }
