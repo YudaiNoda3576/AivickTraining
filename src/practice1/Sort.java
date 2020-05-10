fea48e8e0aedc47e316435b9dd405bedcf787f19
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-10 09:58:15 +0900

    Sort.javaの守勢

--- src/practice1/Sort.java
+++ src/practice1/Sort.java
@@ -4,23 +4,16 @@ import java.util.Arrays;
 
 public class Sort {
 
-	public static  void main(String[] args) {
-	  int[]nums = {3, 2, 10, 5, 20, 13};
-	  int[]nums2 = {100, 50, 30, 20, 10};
-	  
-	  Arrays.sort(nums);
-	  Arrays.sort(nums2);
-	  
-	  for(int i : nums) {
-		  System.out.println(i);
-	  }
-	  
-	  System.out.println("--------------------------");
-	  
-	  for(int j : nums2) {
-		  System.out.println(j);
-	  }
-	  
+	public static void main(String[] args) {
+		int i = 0;
+		int[] array = new int[args.length];
+		for (String arg : args) {
+			array[i] = Integer.parseInt(arg);
+			i++;
+		}
+		Arrays.sort(array);
+		for (int num : array) {
+			System.out.println(num);
+		}
 	}
-
 }
