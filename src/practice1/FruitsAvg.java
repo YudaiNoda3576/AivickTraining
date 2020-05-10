dbff1f9fb504a6a538d0a4fc34f31211fa08890a
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-10 11:31:24 +0900

    FruitsAvgの修正

--- src/practice1/FruitsAvg.java
+++ src/practice1/FruitsAvg.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class FruitsAvg {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		String [] fruitsName = args;
 	
 		Map<String, Integer>fruits = new HashMap<>();
 			fruits.put("りんご", 200);
@@ -16,16 +16,20 @@ public class FruitsAvg {
 			fruits.put("ぶどう", 300);
 			fruits.put("もも", 400);			
 			
-		System.out.println("以下のフルーツの名前を複数入力してください。平均価格を表示します。区切り文字には「,」を使用してください");
-		System.out.println("りんご、バナナ、みかん、ぶどう、もも");
-
-		// 入力値が複数か判定するようにする		
-		String[]fruit = scanner.next().split(",");		
-		int sum = 0;
-		
-		for(int i = 0; i < fruit.length; i++) {
-			sum += fruits.get(fruit[i]);
+		average(fruitsName, fruits);
+		if(args.length == 0) {
+			System.out.println("以下のフルーツの名前を複数入力してください。平均価格を表示します。");
+			System.out.println("りんご、バナナ、みかん、ぶどう、もも");
 		}
-		System.out.println(sum / fruit.length);
+	}
+	
+	public static void average(String[]fruitsName, Map<String, Integer>fruits) {
+		int sum = 0;
+		double ave = 0;
+		for(String fruitName : fruitsName) {
+			sum += fruits.get(fruitName);
+		}
+		ave = sum / fruitsName.length;
+		System.out.println(ave);
 	}
 }
