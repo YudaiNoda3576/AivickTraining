f70d3353478184bd3dc7351be06f9d797b752dc9
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-10 09:58:40 +0900

    Calc.javaの修正

---
+++ src/practice1/Calc.java
@@ -0,0 +1,50 @@
+package practice1;
+
+public class Calc {
+	public static void main(String[] args) {
+		try {
+			String opr = args[0];
+			int num1 = Integer.parseInt(args[1]);
+			int num2 = Integer.parseInt(args[2]);	
+
+			calc(opr, num1, num2);
+		 } catch(ArrayIndexOutOfBoundsException e) {
+			  System.out.println("例外が発生しました。四則演算記号、数値１、数値２を入力してください");
+		  } catch(NumberFormatException e) {
+			  System.out.println("例外が発生しました。不正な値です。");
+		  } catch(IllegalArgumentException e) {
+			  System.out.println("0で割ることはできません");
+		  }
+	}
+	
+	public static void calc(String opr, int num1, int num2) throws 
+		ArrayIndexOutOfBoundsException
+		,NumberFormatException
+		,IllegalArgumentException{
+//		if(opr == "+") {
+//			System.out.println(num1 + num2); 
+//		} else if ( opr == "-") {
+//			System.out.println(num1 - num2); 
+//		} else if ( opr == "m") {
+//			System.out.println(num1 * num2); 
+//		} else if ( opr == "/") {
+//			System.out.println(num1 / num2); 
+//		} else {
+//			System.out.println("入力をやり直してください");
+//		}
+		switch(opr) {
+		case"+":
+			System.out.println(num1 + num2);
+			break;
+		case"-":
+			System.out.println(num1 - num2);
+			break;
+		case"*":
+			System.out.println(num1 * num2);
+			break;
+		case"/":
+			System.out.println(num1 / num2);
+			break;
+		}
+	}
+}
