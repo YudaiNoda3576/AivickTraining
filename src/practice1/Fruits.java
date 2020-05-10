a2772285c38c184eebee25f40f6403f9d1f78169
github user name YudaiNoda3576 <wind.d.r.3576@gmail.com>
2020-05-10 11:31:37 +0900

    Fruits.javaの修正

--- src/practice1/Fruits.java
+++ src/practice1/Fruits.java
@@ -6,27 +6,23 @@ import java.util.Scanner;
 
 public class Fruits {
 
-	public static void main(String[] args) {
-		frp(null);
-	}
-	
-		public static int frp(String fruitName) {
-		Scanner scanner = new Scanner(System.in);
-		int fruitsPrice = 0;
-		
-		Map<String, Integer>fruits = new HashMap<>();
-			fruits.put("りんご", 200);
-			fruits.put("バナナ", 100);
-			fruits.put("みかん", 150);
-			fruits.put("ぶどう", 300);
-			fruits.put("もも", 400);			
+	public static void main(String[] args) {	
+		try {
+			String fruit = args[0];
 			
-		System.out.println("以下のフルーツ名を入力してください。価格を表示します。");
-		System.out.println("りんご、バナナ、みかん、ぶどう、もも");
+			Map<String, Integer>fruits = new HashMap<>();
+				fruits.put("りんご", 200);
+				fruits.put("バナナ", 100);
+				fruits.put("みかん", 150);
+				fruits.put("ぶどう", 300);
+				fruits.put("もも", 400);	
+				
+			System.out.println(fruit + "は" + fruits.get(fruit) + "円です");
+		} catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("例外が発生しました。りんご,バナナ,みかん,ぶどう,もものいずれかを入力してください");
+		}
+				
 		
-		String fruit = scanner.next();
-		System.out.println(fruit + "は" + fruits.get(fruit) + "円です");
 		
-		return fruitsPrice;
 	}
 }
